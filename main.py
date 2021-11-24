89431c2ecfbcb1a871a3f7461b30358a6bfdb2de
Timofey <nikiforov.ta@edu.spbstu.ru>
2021-11-24 14:20:49 +0300

    minor fix

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def main():
                 float(sys.argv[1])
                 for k in range(1, len(sys.argv)):
                     try:
-                        print(f"You entered:\t{float(sys.argv[k])} °F")
-                        print(f"Result:\t{(5 / 9 * (float(sys.argv[k]) - 32)):.3f} °С")
+                        print(f"You entered:\t{float(sys.argv[k])} °C")
+                        print(f"Result:\t{(9 / 5 * float(sys.argv[k]) + 32):.3f} °F")
                     except ValueError:
                         print("Error! Incorrect argument")
             except ValueError:
